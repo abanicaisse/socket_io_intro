441617ee57a5a0347e00b5534869cde720230cc1
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-07-26 14:09:09 +0300

    feat: implement group chat functionality with socketio

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,14 @@
 // console.log(io);
 
 // io() connects to the socket.io sever at the url
-const socket = io("http://localhost:4000");
+const socket = io("http://localhost:4000", {
+  auth: {
+    secret: "This is a secrett",
+  },
+  query: {
+    meaningOfLife: 42,
+  },
+});
 
 // just like on our server, our socket has an
 // on method
@@ -10,4 +17,22 @@ socket.on("welcome", (data) => {
   console.log(data);
   // once welcome is emitted from the server, we run this callback
   socket.emit("thankYou", [4, 5, 6]);
+});
+
+socket.on("newClient", (data) => {
+  console.log("Message to all clients: A new socket has joined", data);
+});
+
+socket.on("msgFromServerToAllClients", (newMessage) => {
+  document.getElementById("messages").innerHTML += `<li>${newMessage}</li>`;
+});
+
+document.getElementById("messages-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const newMessage = document.getElementById("user-message").value;
+  document.getElementById("user-message").value = "";
+
+  // This socket is sending an event to the server...
+  socket.emit("msgFromClientToServer", newMessage);
 });
--- server.js
+++ server.js
@@ -5,19 +5,35 @@ const app = express(); // make an express app
 app.use(express.static("public"));
 const expressServer = app.listen(4000);
 
-const socketio = require("socket.io");
+const { Server } = require("socket.io"); // the object the docs use to make a server
 // io is our socket.io server
-const io = socketio(expressServer, {});
+// Capital S is the server constructor (in the docs, it's the top thing)
+// Small s is the variable in the docs for the server, we call it io
+const io = new Server(expressServer, {
+  cors: ["http://localhost:4000"],
+});
 
 // on is a regular javascript/node event listener
 //emit is the other big method
 io.on("connect", (socket) => {
+  console.log(socket.handshake);
+  // Capital S in the doc, for Socket is the constructor
+  // lower case , socket, is an individual socket
   console.log(socket.id, "has joined our server!");
 
   // firt arg of emit is the event name
-  socket.emit("welcome", [1, 2, 3]); //push an event to the client/browser
+  // socket.emit will emit to THIS one socket
+  // socket.emit("welcome", [1, 2, 3]); //push an event to the client/browser
 
-  socket.on("thankYou", (data) => {
-    console.log("message from client", data);
+  // // io.emit will emit to ALL sockets connected to the server
+  // io.emit("newClient", socket.id);
+
+  // socket.on("thankYou", (data) => {
+  //   console.log("message from client", data);
+  // });
+
+  socket.on("msgFromClientToServer", (newMessage) => {
+    // pass through the message to everyone connected
+    io.emit("msgFromServerToAllClients", newMessage);
   });
 });
